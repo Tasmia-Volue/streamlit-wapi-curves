e2e9ecb4e9e6014ef682bd32d8ca931b894cc0de
Tasmia-Volue <tasmia.zerin@volue.com>
2025-04-23 20:45:15 +0600

    Added config for both curve view

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,27 @@ import pandas as pd
 import wapi_ext as wapi
 from datetime import datetime, timedelta
 import plotly.express as px
+import plotly.graph_objects as go
 
 DEV_API_URLBASE = 'api.wsight.org'
 PROD_API_URLBASE = 'api.volueinsight.com'
 
 output_options = ['entsoe_transparency', 'elhub_actual_production']
 
+dev_color = '#7FE592'
+prod_color = '#E66000'
+
 st.set_page_config(layout="wide")
 
-def create_session(url_base=PROD_API_URLBASE):
+
+def create_session(type: str):
     wapi_ini_read = '/home/tasmia/Documents/Volue/JBOSS/wapi_config.ini'
-    wapi_session = wapi.Session(
-        urlbase=f'https://{url_base}', config_file=wapi_ini_read)
+    if type == 'Development':
+        wapi_session = wapi.Session(
+            urlbase=f'https://{DEV_API_URLBASE}', config_file=wapi_ini_read)
+    else:
+        wapi_session = wapi.Session(
+            urlbase=f'https://{PROD_API_URLBASE}', config_file=wapi_ini_read)
 
     return wapi_session
 
@@ -31,22 +40,33 @@ def format_date(data):
     return data
 
 
-def load_all_curve(session, name):
+def load_all_curve(session, name, keys=None):
     curve_dict = []
     curves = session.get_all_curves_for_script(name)
     curves.sort(key=lambda x: x.name)
     for curve in curves:
-        curve_dict.append(curve)
+        if keys:
+            if all(key in curve.keys for key in keys):
+                curve_dict.append(curve)
+        else:
+            curve_dict.append(curve)
 
     return curve_dict
 
-def load_output_data(session, output):
-    curves = load_all_curve(session, output)
+def load_output_data(session, output, keys=None):
+    curves = load_all_curve(session, output, keys)
     curve_checkbox = {}
     for curve in curves:
         curve_checkbox[curve] = st.checkbox(curve.name)
 
     return curve_checkbox
+
+
+def find_matching_curve_in_dev(curve_name, curves_list):
+    for curve in curves_list:
+        if curve.name == curve_name:
+            return curve
+    return None
 
 
 def load_data(curve, date_from, date_to):
@@ -70,7 +90,9 @@ def create_ui():
     st.title('INSIGHT API :green[VISUALIZATION] 📈')
     st.divider()
 
-    col1, col2 = st.columns([1, 3])
+    col1, col2 = st.columns([1, 4])
+    dev_or_prod = ''
+    both_selected = False
     with col1:
         options = ['Development', 'Production', 'Both']
         dev_prod_selection = st.segmented_control(
@@ -78,22 +100,40 @@ def create_ui():
         )
 
         if dev_prod_selection == 'Development':
-            url_base = DEV_API_URLBASE
+            dev_or_prod = 'Development'
+            url_show = DEV_API_URLBASE
         elif dev_prod_selection == 'Production':
-            url_base = PROD_API_URLBASE
+            dev_or_prod = 'Production'
+            url_show = PROD_API_URLBASE
         else:
-            for url in [DEV_API_URLBASE, PROD_API_URLBASE]:
-                url_base = url
+            dev_or_prod = 'both'
+            url_show = 'Both'
+            both_selected = True
 
-        st.badge(url_base, icon="⚙️", color="green")
+        st.badge(url_show, icon="⚙️", color="green")
 
         output_selected = st.selectbox(
             "Target Output Name:",
             output_options,
         )
 
-        session = create_session(url_base)
-        selected_curves_from_output = load_output_data(session, output_selected)
+        keys = st.text_input("Enter Keys to Filter:", placeholder="e.g. key1, key2")
+        if keys:
+            keys = keys.split(',')
+            keys = [key.strip() for key in keys]
+
+        if both_selected:
+            session = create_session('prod')  # give priority to prod
+        else:
+            session = create_session(dev_or_prod)
+
+        with st.container(height=400, border=True):
+            selected_curves_from_output = load_output_data(session, output_selected, keys) # load all curves for the selected loader
+            if len(selected_curves_from_output) == 0:
+                st.error('No curves exist in Production')
+            if both_selected:
+                selected_curves_from_output_dev = load_all_curve(
+                    session, output_selected, keys)
 
         with col2:
             d_col1, d_col2, d_col3 = st.columns([1, 1, 1])
@@ -105,21 +145,59 @@ def create_ui():
             for selected_curve in selected_curves_from_output:
                 if selected_curves_from_output[selected_curve]:
                     with st.container():
-                        sub_col1, sub_col2 = st.columns([1, 2])
+                        sub_col1, sub_col2 = st.columns([1, 3])
 
                         with sub_col1:
-                            st.write(f':green-background[{selected_curve.name}]')
+                            st.write(':green-background[Curve Details]')
                             selected_curve._metadata = format_date(selected_curve._metadata)
                             df = pd.DataFrame([(key, str(value)) for key, value in selected_curve._metadata.items()], columns=["Attribute", "Value"])
                             st.dataframe(df, hide_index=True)
                         with sub_col2:
                             st.write(f':green[API Curve:]\n _{selected_curve.name}_')
                             curve_data = load_data(selected_curve, d_from, d_to)
-                            # st.line_chart(curve_data['points'], use_container_width=True)
-                            fig = px.line(curve_data['points'], x=curve_data['points'].index, y='value', markers=True)
-                            fig.update_layout(xaxis_title="Date & Time")
-                            fig.update_layout(yaxis_title="Datapoints")
-                            st.plotly_chart(fig, use_container_width=True)
+                            if both_selected:
+                                selected_curve_dev = find_matching_curve_in_dev(selected_curve.name, selected_curves_from_output_dev)
+                                curve_data_dev = load_data(selected_curve_dev, d_from, d_to)
+ 
+                            fig = go.Figure()
+
+                            if both_selected:
+                                fig_curve = go.Scatter(
+                                    x=curve_data['points'].index,
+                                    y=curve_data['points']['value'],
+                                    mode='lines+markers',
+                                    name='Production',
+                                    line=dict(color=prod_color)
+                                )
+                                fig_dev = go.Scatter(
+                                    x=curve_data_dev['points'].index,
+                                    y=curve_data_dev['points']['value'],
+                                    mode='lines+markers',
+                                    name='Development',
+                                    line=dict(color=dev_color)
+                                )
+                                fig.add_trace(fig_curve)
+                                fig.add_trace(fig_dev)
+                            else:
+                                if dev_or_prod == 'Development':
+                                    line = dev_color
+                                else: line = prod_color
+                                fig_curve = go.Scatter(
+                                    x=curve_data['points'].index,
+                                    y=curve_data['points']['value'],
+                                    mode='lines+markers',
+                                    name=f'{dev_or_prod}',
+                                    line=dict(color=line)
+                                )
+                                fig.add_trace(fig_curve)
+
+                            fig.update_layout(
+                                xaxis_title="Date & Time",
+                                yaxis_title="Datapoints",
+                                title="Wapi Curve"
+                            )
+
+                            st.plotly_chart(fig, use_container_width=True, key=curve_data['name'])
 
                     st.divider()
 
